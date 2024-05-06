14b0b7b20d5b040311ceadb567984d99a0367bf4
bin <bininhere@163.com>
2024-05-06 21:57:26 +0800

    Update utils.ts

--- lib/routes/bilibili/utils.ts
+++ lib/routes/bilibili/utils.ts
@@ -5,7 +5,7 @@ import CryptoJS from 'crypto-js';
 function iframe(aid: any, page?: any, bvid?: any) {
     return `<iframe id="myiframe" src="https://www.bilibili.com/blackboard/html5mobileplayer.html?${bvid ? `bvid=${bvid}` : `aid=${aid}`}${
         page ? `&page=${page}` : ''
-    }&high_quality=1"   style="transform: rotate(90deg); transform-origin: 50% 50%;width:460px;height:700px;"  scrolling="yes" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>`;
+    }&high_quality=1"   style="transform: rotate(90deg); transform-origin: 50% 50%;width:700px;height:430px;"  scrolling="yes" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>`;
 }
 
 // a
